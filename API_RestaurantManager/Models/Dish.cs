ec19cd45860b46d63af88eae816fb0c3c3f103f9
wwoi03 <tuandao8826848@gmail.com>
2024-04-01 19:58:18 +0700

    Connect Database + create models

---
+++ API_RestaurantManager/Models/Dish.cs
@@ -0,0 +1,11 @@
+﻿namespace API_RestaurantManager.Models
+{
+    public class Dish
+    {
+        public int DishId { get; set; }
+        public string Name { get; set; }
+        public int MenuId { get; set; }
+        public string DateCreate { get; set; }
+        public string Images { get; set; }
+    }
+}
---
+++ API_RestaurantManager/Models/Menu.cs
@@ -0,0 +1,8 @@
+﻿namespace API_RestaurantManager.Models
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string Name { get; set; }
+    }
+}
